91a252977ca909cfc55082652961b44bf14f8f80
Ebrar ORHAN <144531761+Ebrar-ORHANN@users.noreply.github.com>
2024-05-26 17:01:53 +0300

    2024 Internet Based Programming Projesi

---
+++ projeeee/index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Homepage|Sweetspot</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/0f4915f0d0.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair:wght@300;400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css">
+</head>
+<body>
+    <section id="menu">
+        <div id="logo">SWEETSPOT</div>
+        <div id="year">
+            </div>
+        <nav>
+            <a href="#homepage"><i class="fa-solid fa-house ikon"></i>HOME</a>
+            <a href="#about"><i class="fa-solid fa-circle-info ikon"></i>ABOUT US</a>
+            <a href="#recipes"><i class="fa-solid fa-receipt ikon"></i>DESSERTS</a>
+            <a href="#announcement"><i class="fa-solid fa-bullhorn ikon"></i>ANNOUNCEMENTS</a>
+            <a href="#contact"><i class="fa-solid fa-message ikon"></i>CONTACT US</a>
+            <a href="login.php"><i class="fa-solid fa-user ikon"></i>LOGIN</a>
+            <a href="register.php"><i class="fa-regular fa-user ikon"></i>REGISTER</a>
+        </nav>
+    </section>
+
+    <section id="homepage">
+        <div id="black"></div>
+        <div id="contents">
+            <h2>SWEETSPOT</h2>
+
+            <h3 id="first">WELCOME TO THE ONLY REAL ADDRESS OF TASTE!</h3>
+            <hr width="300" align="left">
+            <p> 
+            </p>
+        </div>
+    </section>
+
+    
+
+    <section id="recipes">
+        <h3 id="h3about">DESSERTS</h3>
+        <div class="container">
+            
+            <div class="owl-carousel owl-theme">
+                <div class="card item" data-merge="1.5">
+                    <img src="images/tatlı3.jpg" alt="" class="img-fluid">
+                    <h5 class="titlecard">CHEESECAKE</h5>
+                    <p class="pcard">
+                       
+                    </p>
+                </div>
+                <div class="card item" data-merge="1.5">
+                    <img src="images/tatlı4.jpg.jpg" alt="" class="img-fluid">
+                    <h5 class="titlecard">WAFFLE</h5>
+                    <p class="pcard">
+                       
+                    </p>
+                </div>
+                <div class="card item" data-merge="1.5">
+                    <img src="images/tatlı1.jpg" alt="" class="img-fluid">
+                    <h5 class="titlecard">CREAMY YOGURT PUDDING</h5>
+                    <p class="pcard">
+                    </p>
+                </div>
+                <div class="card item" data-merge="1.5">
+                    <img src="images/tatlı2.jgp.webp" alt="" class="img-fluid">
+                    <h5 class="titlecard">RICE PUDDING</h5>
+                    <p class="pcard">
+                    </p>
+                </div>
+                <div class="card item" data-merge="1.5">
+                    <img src="images/tatlı5.jpg" alt="" class="img-fluid">
+                    <h5 class="titlecard">CARAMEL TOPPED ICE CREAM DESSERT</h5>
+                    <p class="pcard">
+                      
+                    </p>
+                </div>
+                <div class="card item" data-merge="1.5">
+                    <img src="images/tatlı6.jpg" alt="" class="img-fluid">
+                    <h5 class="titlecard">SUMMER BERRY JELLY</h5>
+                    <p class="pcard">
+            
+                    </p>
+                </div>
+                <div class="card item" data-merge="1.5">
+                    <img src="images/tatlı7.webp" alt="" class="img-fluid">
+                    <h5 class="titlecard">TULUMBA DESSERT</h5>
+                    <p class="pcard">
+                       
+                    </p>
+                </div>
+                <div class="card item" data-merge="1.5">
+                    <img src="images/tatlı8.webp" alt="" class="img-fluid">
+                    <h5 class="titlecard">CHEAT'S MİLLE-FEUILLE</h5>
+                    <p class="pcard">
+                       
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+<section id="about">
+        <h3 id="h3about">ABOUT US</h3>
+        <div class="container">
+            <div id="left">
+                <h5 id="h5left">Sweetspot:  Sweet Delights  Welcome to Sweetspot, where rich heritage meets the irresistible world of cakes
+                    and treats! Our story began in 1978 as a family-run bakery nestled in the heart of the city.  
+                    We weren't just serving treats; we were opening a window to a whole new world of culinary experiences. 
+                    As  and the surrounding  embraced the concept of enjoying coffee alongside sweet treats, the demand for our cakes soared. 
+                    From the very beginning, keeping our customers happy has been our top priority.
+                    For generations, we've worked six days a week to ensure every visit is a delightful one, filled with the aroma of freshly
+                    baked goods and the joy of satisfying a sweet tooth.
+                    Step into our shop and discover the sweet spot of your day!</h5>
+            </div>
+           
+
+     <img src="images/about_2.png"alt="" class="img-fluid mt100">
+
+            
+        </div>
+    </section>
+    <section id="announcement">
+        <h3 id="h3about">ANNOUNCEMENTS</h3>
+        <div class="container">
+            <div class="owl-carousel owl-theme">
+                <div class="card item" data-merge="1.5">
+                    <img src="images/announcement1.jpg" alt="" class="img-fluid">
+                    <h5 class="titlecard">New Dessert</h5>
+                    <p class="cardp">
+                    OUR STRAWBERRY TART WILL BE WITH YOU SOON
+                    </p>
+                </div>
+                <div class="card item" data-merge="1.5">
+                    <img src="images/announcement2.jpg" alt="" class="img-fluid">
+                    <h5 class="titlecard">New Branch</h5>
+                    <p class="cardp">
+                    We will be with you very soon with our Safranbolu branch.
+                    </p>
+                </div>
+                <div class="card item" data-merge="1.5">
+                    <img src="images/announcement_3.jpg" alt="" class="img-fluid">
+                    <h5 class="titlecard">WIN</h5>
+                    <p class="cardp">
+                    The results of this month's dessert contest have been announced.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="contact">
+        <h3 id="h3about">CONTACT US</h3>
+        <div class="container">
+            <form action="index.php" method="post">
+            <div id="contactopac">
+                <div id="formgroup">
+                    <div id="leftform">
+                        <input type="text" name="name" placeholder="Name" required class="form-control">
+                        <input type="text" name="surname" placeholder="Surname" required class="form-control">
+                    </div>
+                    <div id="rightform">
+                        <input type="email" name="mail" placeholder="E-mail" required class="form-control">
+                        <input type="text" name="topic" placeholder="Topic" required class="form-control">
+                    </div>
+
+                    <textarea name="message" id="" cols="30" rows="10" placeholder="Write Message" required class="form-control"></textarea>
+
+                    <input type="submit" value="SUBMIT">
+                </div>
+                <div id="address">
+                    <h4 id="addresstitle">Address : </h4>
+                    <p class="addressp">Mutlu Mahallesi</p>
+                    <p class="addressp">Endam Caddesi</p>
+                    <p class="addressp"> 2501.Sokak No:6</p>
+                    <p class="addressp">0212 252 501 99</p>
+                    <p class="addressp">Email:sweetspot@sweetspot.com</p>
+                </div>
+            </div>
+            </form>
+            <footer>
+                <div id="copyright">2024 | All Rights Reserved</div>
+                <a href="#menu"><i class="fa-solid fa-circle-up" id="up"></i></a>
+            </footer>
+
+        </div>
+    </section>
+    <script
+    src="https://code.jquery.com/jquery-3.7.0.slim.min.js"
+    integrity="sha256-tG5mcZUtJsZvyKAxYLVXrmjKBVLd6VpVccqz/r4ypFE="
+    crossorigin="anonymous"></script>    
+    <script src="owl/owl.carousel.min.js"></script>
+    <script src="owl/script.js"></script>
+</body>
+</html>
+
+<?php
+
+include("connection.php");
+
+if (isset($_POST["name"], $_POST["surname"], $_POST["mail"], $_POST["topic"], $_POST["message"]))
+{
+    $name=$_POST["name"];
+    $surname=$_POST["surname"];
+    $mail=$_POST["mail"];
+    $topic=$_POST["topic"];
+    $message=$_POST["message"];
+
+    $insert="INSERT INTO messages (f_name, l_name, email, topic, message) VALUES ('".$name."', '".$surname."','".$mail."','".$topic."','".$message."')";
+
+    if ($conn->query($insert)===TRUE){
+        echo "<script>alert('Your message has been sent successfully.')</script>";
+    }
+}
+?>
